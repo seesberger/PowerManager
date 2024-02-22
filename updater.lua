33de2b1b6011a7d94f04ac831626bf92aeecb6e6
Frederick Meisel <meisel.frederick@gmx.de>
2024-02-22 18:10:12 +0100

    -

--- updater.lua
+++ updater.lua
@@ -1,1 +1,12 @@
-os.execute("gitrepo seesberger/PowerManager /home/PowerManager")
+
+print("Updaten? (Y/*)")
+local abfrage = term.read()
+
+if abfrage == "Y" then
+    print("Ja!")
+    --os.execute("gitrepo seesberger/PowerManager /home/PowerManager")
+else
+    print("Nein!")
+    os.sleep(1)
+end
+
