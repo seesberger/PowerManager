492606703c4d0888f833c11feb6a1147a3d5745d
Frederick Meisel <meisel.frederick@gmx.de>
2024-02-23 17:31:28 +0100

    added update functionality from powerman update <arguments>

--- shortcut.lua
+++ shortcut.lua
@@ -11,9 +11,13 @@ if args[1] == "-h" then
     print(helpText)
     return
 elseif args[1] == "update" then
+    if #args == 2 then
+        os.execute("/usr/PowerManager/updater.lua "..args[2])
+        return
+    end
     os.execute("/usr/PowerManager/updater.lua -a")
     return
 else
-    print('"'..args[1]..'" - Bad argument. you can use: powerman update')
+    print('"'..args[1]..'" - Bad argument. you can use: powerman update <argument>')
     return
 end
