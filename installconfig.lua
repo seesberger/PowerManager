2e0de2dcd20e5c973548164b1f1b7a97e6ae86e9
unknown <boehnrobin@gmail.com>
2024-03-03 13:22:48 +0100

    test version

--- installconfig.lua
+++ installconfig.lua
@@ -9,13 +9,24 @@ Config = {
 --    ManifestTarget = "/etc/manifest/", -- should maybe be "/home/.git-tool/"
 --    ShortcutTargetDir = "/usr/bin/",
 --    RunOnBoot = false
-    Installation = {        
+    Installation = {
         Shortcut = {
+            Source = "shortcut.lua",
             Name = "powerman",
             Taregt = "/usr/bin/" -- target for file shortcut.lua
         },
-        Files = {},
-
+        TargetDirectory = "/usr/bin/powerman",
+        Files = {
+            "desktopApplication.lua",
+            "git-tool.lua",
+            "main.lua",
+            "settings.lua",
+            "applications/applications.lua",
+            "applications/applicationTemplate.lua",
+            "applications/energyCellReadout.lua",
+            "applications/fickDichMeter.lua",
+            "applications/settings.lua"
+        }
     }
 }
 
@@ -30,21 +41,48 @@ Repository = {
 }
 
 Dependencies = {
-    GutApi = {
-        Type = "Script",
-        -- fixme : instert repo here
-    }
-    -- GuiApi = {
-    --     Type = "IntallFiles",
-    --     Loose = false,
-    --     Files = {
-    --         {
-    --             URL = "",
-    --             Target = ""
-    --         },
-    --         {}
+    -- GutApi = {
+    --     Type = "Repository",
+    --     -- fixme : instert repo here
+    --     Name  = "GUI API",
+    --     Repository = {
+    --         Owner = "seesberger",
+    --         Name = "OpenComputers-GUI",
+    --         ShortName = "ocgui",
+    --         RepoIdentifier = "seesberger/OpenComputers-GUI",
+    --         Remote = SupportedRemotes.Github,
+    --         CurrentBranch = "master"
     --     }
     -- }
+    GuiApi = {
+        Type = "IntallFiles",
+        Loose = false, -- not implemented
+        Files = {
+            {
+                URL = "https://raw.githubusercontent.com/seesberger/OpenComputers-GUI/master/doubleBuffering.lua",
+                Target = "/lib/doubleBuffering.lua"
+            },
+            {
+                URL = "https://raw.githubusercontent.com/seesberger/OpenComputers-GUI/master/image.lua",
+                Target = "/lib/image.lua"
+            },{
+                URL = "https://raw.githubusercontent.com/seesberger/OpenComputers-GUI/master/color.lua",
+                Target = "/lib/doubleBuffering.lua"
+            },{
+                URL = "https://raw.githubusercontent.com/seesberger/OpenComputers-GUI/master/advancedLua.lua",
+                Target = "/lib/advancedLua.lua"
+            },
+            {
+                URL = "https://raw.githubusercontent.com/seesberger/OpenComputers-GUI/master/GUI.lua",
+                Target = "/lib/GUI.lua"
+            },
+            {
+                URL = "https://raw.githubusercontent.com/seesberger/OpenComputers-GUI/master/FormatModules/OCIF.lua",
+                Target = "/lib/FormatModules/OCIF.lua",
+                MakeDir = "/lib/FormatModules/"
+            }
+        }
+    }
 }
 
 return Config, Repository, Dependencies
